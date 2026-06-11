a10756509febc97517f0a34066313db018f6deb7
Harsha Arya <harsharya195@gmail.com>
2026-06-11 22:59:51 +0530

    Add RainbowKit multi-wallet support

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -114,7 +114,7 @@ export default function Hero() {
                     <rect x="2" y="7" width="20" height="14" rx="2" ry="2" />
                     <path d="M16 21V5a2 2 0 00-2-2h-4a2 2 0 00-2 2v16" />
                   </svg>
-                  Connect MetaMask
+                  Connect Wallet
                 </span>
               )}
             </button>
